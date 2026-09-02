b44be927d1fbf60e21cf7a67b0fe502b778c7738
A N M Talha <talhanaim875468@gmail.com>
2026-09-02 12:55:19 +0600

    refactor: improve application error structure

--- src/app/errors/AppError.ts
+++ src/app/errors/AppError.ts
@@ -1,10 +1,12 @@
 class AppError extends Error {
   public statusCode: number;
+  public isOperational: boolean;
 
   constructor(statusCode: number, message: string) {
     super(message);
 
     this.statusCode = statusCode;
+    this.isOperational = true;
 
     Error.captureStackTrace(this, this.constructor);
   }
